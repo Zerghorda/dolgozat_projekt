c6e1d65b4aba63e8749729c9c4fc09739025e302
Tar Gergő <tar.gergo@dk.akkszalezi.hu>
2024-12-09 13:36:35 +0100

    3. feladat

--- src/main/java/hu/szamalk/Modell/Tabla.java
+++ src/main/java/hu/szamalk/Modell/Tabla.java
@@ -1,5 +1,7 @@
 package hu.szamalk.Modell;
 
+import java.util.Arrays;
+
 public class Tabla {
     private String [][] t;
     private char UresCella;
@@ -8,5 +10,12 @@ public class Tabla {
         this.t = new String[8][8];
         UresCella = uresCella;
     }
-    
+    //3. feladat 3.része
+    public void tablaTolt(){
+        for (int i =0;i<t.length;i++){
+            for (int j = 0;j<t[i].length;j++){
+                Arrays.fill(t[j], this.UresCella);
+            }
+        }
+    }
 }
